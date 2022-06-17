1eb20c347ba34a319292bd01116a3848db57133c
cotiknbtsFiles <107369455+cotiknbtsFiles@users.noreply.github.com>
2022-06-18 04:42:30 +0700

    Add files via upload

---
+++ tagger.py
@@ -0,0 +1,41 @@
+from asyncio import sleep
+from .. import loader, utils
+
+def register(cb):
+	cb(TaggerMod())
+	
+class TaggerMod(loader.Module):
+	"""Часто отмечает выбранного пользователя"""
+	strings = {'name': 'Tagger by cotik_'}
+	def __init__(self):
+		self.name = self.strings['name']
+		self._me = None
+		self._ratelimit = []
+		
+	async def taggercmd(self, message):
+		""".tagger <колличество> <реплей>"""
+		reply = await message.get_reply_message()
+		if not reply:
+			await message.edit("<b>Зачем звал, тут же никого нет? >=(</b>")
+			return
+		id = reply.sender_id
+		args = utils.get_args(message)
+		count = 50
+		if args:
+			if args[0].isdigit():
+				if int(args[0]) < 0:
+					count = 50
+				else:
+					count = int(args[0])
+		txt = '<a href="tg://user?id={}">{}</a>'
+		await message.edit(txt.format(id, "Ну что, погнали!)"))
+		for _ in range(count):
+			await sleep(0.3)
+			msg = await message.client.send_message(message.to_id, txt.format(id, "Ответь пжпж 😘"), reply_to=message)
+			if not msg.is_reply:
+				await msg.edit("<b>Ладно, ладно, только не злись!</b>")
+				break
+			await sleep(0.3)
+			await msg.delete()
+				
+			
